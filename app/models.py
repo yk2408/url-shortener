983e469f6b248ca4f397a3809a1ecab9ba523fcb
Yash Khatri <yash.keepgrowing@gmail.com>
2024-11-13 13:53:46 +0530

    Added URLs model

--- app/models.py
+++ app/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+class Urls(models.Model):
+    full_url = models.CharField(max_length=100)
+    shortened_url = models.CharField(max_length=100, null=True)
+    clicks = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.full_url)
